6c46d556b346f26075a456f6b136a2fd1d1e374b
Dhawal Singh Panwar <dsp9107@outlook.com>
2019-09-13 14:20:29 +0530

    Copies Stripped Content To Clipboard

--- prepGFG.py
+++ prepGFG.py
@@ -1,9 +1,17 @@
-OUT = open('OUT.txt','w') 
+import re
+import pyperclip
+
+OUT = open('OUT.txt','r+') 
 
 with open("IN.txt", "r") as IN:
     data = IN.readlines()
     for line in data:
-        print(line)
-        OUT.write(line.rstrip()+"\n")
+        x = re.search("^(\s)*/+.*",line)
+        if x == None :
+            print(line.rstrip())
+            OUT.write(line.rstrip()+"\n")
+
+OUT.seek(0,0)
+pyperclip.copy(OUT.read())
 
 OUT.close()
